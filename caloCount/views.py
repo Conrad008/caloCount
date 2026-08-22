085a9c40628cc1b2821f6f3966d5ad327164e48f
Conrad008 <kipngeno7180@gmail.com>
2026-08-22 20:30:13 +0300

    feat: add index view to list food items, compute total calories, and handle form submission

--- caloCount/views.py
+++ caloCount/views.py
@@ -3,3 +3,20 @@ from django.shortcuts import render , redirect, get_object_or_404
 from django.db.models import Sum
 from .models import FoodItem
 from .forms import FoodItemForm
+
+def index(request):
+    food_items = FoodItem.objects.all()
+    total_calories = food_items.aggregate(Sum('calories'))['calories__sum'] or 0
+    form = FoodItemForm()
+
+    if request.method == 'POST':
+        form = FoodItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+
+    return render(request, 'calorie_tracker/index.html', {
+        'food_items': food_items,
+        'total_calories': total_calories,
+        'form': form,
+    })
