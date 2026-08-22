b3773685bf408e5e431c2dae2b721e50083efc4b
Conrad008 <kipngeno7180@gmail.com>
2026-08-22 20:22:57 +0300

    refactor: update imports in views for FoodItem CRUD operations

--- caloCount/views.py
+++ caloCount/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect, get_object_or_404
 
-# Create your views here.
+from django.db.models import Sum
+from .models import FoodItem
+from .forms import FoodItemForm
